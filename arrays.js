fe50e557837471c8005a1632e1291446ce9cad05
Carly Pecora <carlyannpecora@gmail.com>
2019-03-25 00:19:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,5 +4,9 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
 function 
 
